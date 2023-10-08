8237c0b0ddea343139281af7e4b0f6d117be3846
Gabriel Reyes <114525321+GabrielReyes16@users.noreply.github.com>
2023-10-08 04:17:22 -0500

    Update urls.py

--- Django/life_far_away/app/urls.py
+++ Django/life_far_away/app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlspatters = [
-    path('', views.index, name='index'),
+   # path('', views.index, name='index'),
    # path('planet/', views.planet, name='planet'),
 ]
