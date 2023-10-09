83d20dfca9befe3398e8c610dbb81a727c61a0e1
GabrielReyes16 <gabriel.reyes.b@tecsup.edu.pe>
2023-10-08 19:58:07 -0500

    Urls and views

--- Django/life_far_away/app/urls.py
+++ Django/life_far_away/app/urls.py
@@ -3,7 +3,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name = "index"),
     path('planet', views.planet, name = "planet"),
-    # path('planet/data', views.data, name = "data"),
+    path('planet/data', views.data, name = "data"),
     # path('planet/'),
    #  path('producto', views.producto, name = "producto"),
 ]
--- Django/life_far_away/app/views.py
+++ Django/life_far_away/app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'planet.html')
 
 def planet (request):
 # def planet (request,codigo):
@@ -12,3 +12,6 @@ def planet (request):
     # }
     return render(request,"planet.html")
     # return render(request,"planet.html",context)
+
+def data(request):
+    return render(request, 'planet_data.html')
