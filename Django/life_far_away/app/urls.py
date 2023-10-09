99d99600382eab410b31df75da33179910143109
josearcos2023 <jose.arcos@tecsup.edu.pe>
2023-10-08 19:19:57 -0500

    2do html

--- Django/life_far_away/app/urls.py
+++ Django/life_far_away/app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name = "index"),
-    # path('planet', views.planet, name = "planet"),
+    path('planet', views.planet, name = "planet"),
     # path('planet/data', views.data, name = "data"),
     # path('planet/'),
    #  path('producto', views.producto, name = "producto"),
--- Django/life_far_away/app/views.py
+++ Django/life_far_away/app/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def planet (request):
+# def planet (request,codigo):
+    # planeta=Planeta.objects.get(id=codigo)
+    # context={
+    #     'planeta':planeta
+    # }
+    return render(request,"planet.html")
+    # return render(request,"planet.html",context)
